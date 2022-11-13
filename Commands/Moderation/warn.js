512f534c50da7d66ae7c8c3f299c2a019296501b
Sarthak Kundu <74956651+dragoluca22@users.noreply.github.com>
2022-11-13 11:10:59 +0530

    Fixed emojis in warn

--- Commands/Moderation/warn.js
+++ Commands/Moderation/warn.js
@@ -76,8 +76,8 @@ module.exports = {
 
                 const Member = guild.members.cache.get(member.id)
 
-                if (Member.id === user.id) return Reply(interaction, process.env.Cross, `You can't warn yourself!`, true)
-                if (guild.ownerId === Member.id) return Reply(interaction, process.env.Cross, `You can't warn the server's owner!`, true)
+                if (Member.id === user.id) return Reply(interaction, "❌", `You can't warn yourself!`, true)
+                if (guild.ownerId === Member.id) return Reply(interaction, "❌", `You can't warn the server's owner!`, true)
 
                 await interaction.deferReply()
 
@@ -93,12 +93,12 @@ module.exports = {
 
                 await Data.save()
 
-                EditReply(interaction, process.env.Tick, `${Member} has been warned for: **${reason}**`)
+                EditReply(interaction, "✅", `${Member} has been warned for: **${reason}**`)
 
                 Member.send({
                     embeds: [
                         new EmbedBuilder()
-                            .setTitle(`${process.env.Warning} | You have been Warned!`)
+                            .setTitle(`⚠ | You have been Warned!`)
                             .setColor(client.color)
                             .setThumbnail(Member.displayAvatarURL())
                             .addFields(
@@ -125,13 +125,13 @@ module.exports = {
                 const warnid = options.getString("warn-id")
 
                 const data = await DB.findOne({ _id: warnid }).catch(err => { })
-                if (!data) return EditReply(interaction, process.env.Cross, `The ID you provided is not a valid ID!`)
+                if (!data) return EditReply(interaction, "❌", `The ID you provided is not a valid ID!`)
 
                 const member = guild.members.cache.get(data.User)
 
                 await data.delete()
 
-                EditReply(interaction, process.env.Tick, `Removed \`1\` warning from **${member}**`)
+                EditReply(interaction, "✅", `Removed \`1\` warning from **${member}**`)
 
             }
                 break;
@@ -139,11 +139,11 @@ module.exports = {
             case "list": {
 
                 const member = options.getMember("user")
-                if (!member) return Reply(interaction, process.env.Cross, `The member couldn't be found!`)
+                if (!member) return Reply(interaction, "❌", `The member couldn't be found!`)
                 const Member = guild.members.cache.get(member.id)
 
                 const userWarns = await DB.find({ User: Member.id, Guild: guild.id }).catch(err => { })
-                if (userWarns.length === 0) return Reply(interaction, process.env.Cross, `**${Member}** does not have any warnings!`)
+                if (userWarns.length === 0) return Reply(interaction, "❌", `**${Member}** does not have any warnings!`)
 
                 const embeds = warnlistEmbed(userWarns, 5, guild, Member, client)
                 Pagination(interaction, embeds, user)
